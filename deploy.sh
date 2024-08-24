53e3241a22288ccb5dd285c76a28d9e03c1a7e4b
anszhao <anszhao@163.com>
2024-08-24 15:22:27 +0800

    Add files via upload
    deploy script upload v1

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#Install fast plus google bbr software and run it on server
+
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+#Install SS server from git
+
+wget — no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
+
+#Change ss to x
+chmod +x shadowsocks.sh
+
+#Run deploy script and install ss server
+
+./shadowsocks.sh 2>&1 | tee shadowsocks.log
+
+#Replace python to python3
+
+sed -i 's/python setup/python3 setup/g' shadowsocks.sh
+
+#Change 'collections.MutableMapping' to 'collections.abc.MutableMapping'
+sed -i 's/collections.MutableMapping/collections.abc.MutableMapping/g' lru_cache.py /usr/local/lib/python3.12/dist-packages/shadowsocks-3.0.0-py3.12.egg/shadowsocks/lru_cache.py
+
+#Open port xxx for this server
+ufw allow 8877
+
+#Restart SS server
+/etc/init.d/shadowsocks restart
+
+/etc/init.d/shadowsocks status
+
+echo "SS server deploy completed!!!, now you can use it!"
